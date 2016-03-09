40653287ed206785fe63fbb8883622f2103ed19d
marco <marco@webguerilla.net>
2016-03-09 22:16:43 +0100

    finished implementation; added license

--- src/plugin/html5audio.php
+++ src/plugin/html5audio.php
@@ -2,4 +2,20 @@
 defined ('_JEXEC') or die ('Restricted access');
 
 class plgContentHtml5audio extends JPlugin {
+	protected $autoloadLanguage = true;
+
+	public function onContentPrepare($context, &$article, &$params, $page = 0) {
+		$pattern = '/{audio}(.*){\/audio}/';
+		$matches = array();
+
+		$ok = preg_match_all($pattern, $article->text, $matches, PREG_SET_ORDER);
+		if (!$ok) {
+			return;
+		}
+
+		foreach($matches as $match) {
+			$replacement = sprintf('<audio controls src="%s">%s</audio>', $match[1], JText::_('PLG_CONTENT_HTML5AUDIO_NOT_SUPPORTED'));
+			$article->text = str_replace($match[0], $replacement, $article->text);
+		}
+	}
 }
