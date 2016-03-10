259e290077f9639f1e9dd76c2540c03b7317e933
marco <marco@webguerilla.net>
2016-03-10 21:45:19 +0100

    added base path param

--- src/plugin/html5audio.php
+++ src/plugin/html5audio.php
@@ -14,7 +14,7 @@ class plgContentHtml5audio extends JPlugin {
 		}
 
 		foreach($matches as $match) {
-			$filepath = htmlspecialchars($match[1]); // prevents XSS
+			$filepath =  htmlspecialchars($this->params->get('base_path', '') . $match[1]); // prevents XSS
 
 			$replacement = sprintf('<audio controls src="%s">%s</audio>', $filepath, JText::_('PLG_CONTENT_HTML5AUDIO_NOT_SUPPORTED'));
 			$article->text = str_replace($match[0], $replacement, $article->text);
